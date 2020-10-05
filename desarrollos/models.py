8298cc64191f49c56bf0218072a53e29aa16c7e3
abarreracon <abarreracon@gmail.com>
2020-10-04 23:57:29 -0500

    setting up configs project, creating desarrollo and leads models

--- desarrollos/models.py
+++ desarrollos/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Desarrollo(models.Model):
+    nombre_desarrollo = models.CharField(max_length=100)
+    ubicacion_desarrollo = models.CharField(max_length=150)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    leads = models.ManyToManyField('Leads')
+
+    def __str__(self):
+        return self.nombre_desarrollo
+
+class Leads(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=30, blank=False, unique=True)
+    email = models.EmailField(max_length=250)
+
+    def __str__(self):
+        return '%s %s' % (self.nombre, self.apellido)
